cf9879d415fe0ae5a7ee90f84a912119f6a7ffce
vaishu-dd45 <vaishnvirahangdale123@gmail.com>
2025-12-10 16:01:07 +0530

    Initial commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,15 +4,15 @@ import Nod from "./components/Node";
 import Bye from "./components/bye";
 import Array from "./components/array";
 import Memo from "./components/memo";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Hello/>
-      <Hi/>
-      <Nod/>
-      <Bye/>
-      <Array/>
-      <Memo  />
+    <Hi/>
+    <Routes>  
+      <Route path="/array" element={<Array/>}/>
+      <Route path="/memo" element={<Memo/>}/>
+    </Routes>
     </div>
   );
 }
--- my-react-app/src/components/hi.jsx
+++ my-react-app/src/components/hi.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import  "../components/hi.css";
+import {Link,NavLink} from "react-router-dom";
 const hi = () => {
   return (
-    <div>
+    <div className='box'>
       <h1> hellloooo kya kr rhe ho </h1>
+      <nav>
+        <Link to="/Array">Array</Link>
+      </nav>
+      <NavLink to="/Memo" style={({isActive}) =>({color:isActive? "red" :"black"})}>MEMO</NavLink>
     </div>
   )
 }
 
-export default hi
+export default hi;
